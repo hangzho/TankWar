3437090993a51e438f16a20eb9f599a745afb272
Hang <johanzhou8@gmail.com>
2014-12-26 21:36:33 -0500

    TankWar0.2 Functions: Be able to close the window. Do not allow to change the size of window.
    功能：
    添加关闭窗口事件。
    不允许改动窗口。
    掌握：
    匿名类的写法（直接new）。
    重写父类方法。

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,10 +1,18 @@
 import java.awt.Frame;
+import java.awt.event.*;
 
 public class TankClient extends Frame {
 
 	public void launchFrame(){
 		this.setLocation(400, 300);
 		this.setSize(800, 600);
+		this.setTitle("TankWar");
+		this.addWindowListener(new WindowAdapter(){
+
+			public void windowClosing(WindowEvent e) {//重写了父类中的函数
+				System.exit(0);
+			}});
+		this.setResizable(false);
 		this.setVisible(true);
 	}
 	
