5836e16dd4f76c2f04b2a8fd47c045963bca100b
Hang <johanzhou8@gmail.com>
2014-12-26 23:05:12 -0500

    TankWar0.4 Tank can move. 让坦克动起来

--- src/TankClient.java
+++ src/TankClient.java
@@ -5,13 +5,15 @@ import java.awt.event.*;
 
 public class TankClient extends Frame {
 
-
+	int x = 50, y = 50;//控制位置
+	
 	public void paint(Graphics g) {//初始化时，paint会被自动调用
 		Color c = g.getColor();//g 是前景色
 		g.setColor(Color.RED);
-		g.fillOval(50, 50, 30, 30);//x,y为左顶角，后面两个函数为宽和高
+		g.fillOval(x, y, 30, 30);//x,y为左顶角，后面两个函数为宽和高
 		g.setColor(c);//还回去
 		
+		y += 5;
 	}
 
 	public void launchFrame(){
@@ -27,11 +29,28 @@ public class TankClient extends Frame {
 		this.setResizable(false);
 		this.setBackground(Color.GREEN);
 		this.setVisible(true);
+		new Thread(new PaintThread()).start();//启动线程重画
 	}
 	
 	public static void main(String[] args) {
 		TankClient tc= new TankClient();
 		tc.launchFrame();
+	}
+	
+	private class PaintThread implements Runnable{//线程 由内部类完成
+
+		public void run() {
+			while(true){
+				repaint();//父类中的，内部调用paint方法
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
 	}
 
 }
