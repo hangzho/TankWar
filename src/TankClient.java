563d7a08b0513ac92f48ce1ec24d4669275f73fa
Hang <johanzhou8@gmail.com>
2014-12-26 22:51:30 -0500

    TankWar0.3 Draw a circle.
    画了同心圆

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,7 +1,18 @@
+import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.event.*;
 
 public class TankClient extends Frame {
+
+
+	public void paint(Graphics g) {//初始化时，paint会被自动调用
+		Color c = g.getColor();//g 是前景色
+		g.setColor(Color.RED);
+		g.fillOval(50, 50, 30, 30);//x,y为左顶角，后面两个函数为宽和高
+		g.setColor(c);//还回去
+		
+	}
 
 	public void launchFrame(){
 		this.setLocation(400, 300);
@@ -11,8 +22,10 @@ public class TankClient extends Frame {
 
 			public void windowClosing(WindowEvent e) {//重写了父类中的函数
 				System.exit(0);
-			}});
+			}
+			});
 		this.setResizable(false);
+		this.setBackground(Color.GREEN);
 		this.setVisible(true);
 	}
 	
