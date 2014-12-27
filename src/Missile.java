45a07d2c6979430e01a71e35716ab2655fb789c3
Hang <johanzhou8@gmail.com>
2014-12-27 12:30:40 -0500

    TankWar 1.0 Add a missile class. Add some method into the class. 添加missile类， 包括构造函数，draw，move和一些属性

---
+++ src/Missile.java
@@ -0,0 +1,62 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+public class Missile {
+
+	int x, y;//位置
+	Tank.Direction dir;//方向
+	public static final int XSPEED = 10;
+	public static final int YSPEED = 10;
+
+	public Missile(int x, int y, Tank.Direction dir) {
+		this.x = x;
+		this.y = y;
+		this.dir = dir;
+	}	
+	
+	public void draw(Graphics g){
+		Color c = g.getColor();
+		g.setColor(Color.BLACK);
+		g.fillOval(x, y, 10, 10);
+		g.setColor(c);
+		
+		move();
+	}
+
+	private void move() {
+		// TODO Auto-generated method stub
+		switch(dir){
+		case L:
+			x -= XSPEED;
+			break;
+		case LU:
+			x -= XSPEED;
+			y -= YSPEED;
+			break;
+		case U:
+			y -= YSPEED;
+			break;
+		case RU:
+			x += XSPEED;
+			y -= YSPEED;
+			break;
+		case R:
+			x += XSPEED;
+			break;
+		case RD:
+			x += XSPEED;
+			y += YSPEED;
+			break;
+		case D:
+			y += YSPEED;
+			break;
+		case LD:
+			x -= XSPEED;
+			y += YSPEED;
+			break;
+		}
+	}
+	
+	
+}
