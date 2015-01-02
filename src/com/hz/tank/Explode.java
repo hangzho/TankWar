9513c98af9c7727afbcf3fb0b4a5553c39f9c1d7
Hang <johanzhou8@gmail.com>
2015-01-02 11:26:05 -0500

    TankWar 2.8 Add images to missile class. Utilize Java reflection.

--- src/com/hz/tank/Explode.java
+++ src/com/hz/tank/Explode.java
@@ -6,7 +6,23 @@ public class Explode {
 	int x, y;
 	private boolean live = true;
 	private TankClient tc;
-	int[] diameter = { 4, 7, 12, 18, 26, 32, 49, 30, 14, 6 };
+	//int[] diameter = { 4, 7, 12, 18, 26, 32, 49, 30, 14, 6 };
+	
+	private static Toolkit tk = Toolkit.getDefaultToolkit();
+	
+	private static Image[] images = {  //don't have to load images every time using static 
+			tk.getImage(Explode.class.getClassLoader().getResource("images/0.gif")),//reflection
+			tk.getImage(Explode.class.getClassLoader().getResource("images/1.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/2.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/3.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/4.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/5.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/6.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/7.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/8.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/9.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource("images/10.gif"))
+	};
 	int step = 0;
 
 	public Explode(int x, int y, TankClient tc) {
@@ -20,15 +36,12 @@ public class Explode {
 			tc.explodes.remove(this);
 			return;
 		}
-		if (step == diameter.length) {
+		if (step == images.length) {
 			live = false;
 			step = 0;
 			return;
 		}
-		Color c = g.getColor();
-		g.setColor(Color.ORANGE);
-		g.fillOval(x, y, diameter[step], diameter[step]);
-		g.setColor(c);
+		g.drawImage(images[step], x, y, null);
 		step++;
 	}
 }
